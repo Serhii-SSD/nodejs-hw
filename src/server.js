86220b14c0b29133ed4e911e3d94458c09d38451
Serhii-SSD <demydenko.ss@gmail.com>
2026-09-20 16:03:26 +0300

    feat: implement express server, routes, middleware and error handling

---
+++ src/server.js
@@ -0,0 +1,54 @@
+
+import 'dotenv/config';
+import express from "express";
+import cors from 'cors';
+import pinoHttp from 'pino-http';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(pinoHttp({
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+    },
+  },
+}));
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/notes", (req, res) => {
+  res.status(200).json({
+    message: "Retrieved all notes"
+  });
+});
+
+app.get("/notes/:noteId", (req, res) => {
+  const id_param = req.params.noteId;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${id_param}`
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "Route not found"
+  });
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message || "Internal Server Error"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port ${PORT}")
+});
